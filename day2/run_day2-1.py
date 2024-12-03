d0ae75ac433cfae3f9259dc723bca389c1d28648
Guilherme Ribeiro <guiribeiro.0228@gmail.com>
2024-12-03 09:23:46 +0000

    Refactor day 2 part 1

--- day2/run_day2-1.py
+++ day2/run_day2-1.py
@@ -3,23 +3,20 @@ with open(f"input.txt") as file:
     lines = file.readlines()
 
 
-def is_decreasing(lst):
-    return all(a > b for a, b in zip(lst, lst[1:]))
 
-def is_increasing(lst):
-    return all(a < b for a, b in zip(lst, lst[1:]))
-
-def check_differences(lst):
-    return all(1 <= abs(a - b) <= 3 for a, b in zip(lst, lst[1:]))
+def is_safe(split_line) -> bool:
+    if split_line == sorted(split_line, reverse=True) or split_line == sorted(split_line):
+        if all(1 <= abs(a - b) <= 3 for a, b in zip(split_line, split_line[1:])):
+            return True
+    return False
 
 
 def main():
     safe_count = 0
     for line in lines:
         split_line = list(map(int, line.split()))
-        if (is_decreasing(split_line) or is_increasing(split_line)):
-            if check_differences(split_line):
-                safe_count += 1
+        if is_safe(split_line):
+            safe_count += 1
     print(f"Count of safe reports: {safe_count}")
 
 
--- day2/run_day2-2.py
+++ day2/run_day2-2.py
@@ -3,25 +3,19 @@ with open(f"input.txt") as file:
     lines = file.readlines()
 
 
-def is_safe(lst) -> bool:
-    
+def is_safe(split_line) -> bool:
+    if split_line == sorted(split_line, reverse=True) or split_line == sorted(split_line):
+        if all(1 <= abs(a - b) <= 3 for a, b in zip(split_line, split_line[1:])):
+            return True
+    return False
 
 
 def main():
     safe_count = 0
     for line in lines:
         split_line = list(map(int, line.split()))
-        if split_line != sorted(split_line) or split_line != sorted(split_line, reverse=True)
-        
-        for a, b in zip(split_line, split_line[1:]):
-            if not(a > b)
-        
-        if (is_decreasing(split_line) or is_increasing(split_line)):
-            for a, b in zip(split_line, split_line[1:]):
-                if  not(1 <= abs (a - b) <= 3):
-                    tolerate += 1
-            if tolerate < 2 and is_safe:
-                safe_count += 1
+        if is_safe(split_line):
+            safe_count += 1
     print(f"Count of safe reports: {safe_count}")
 
 
